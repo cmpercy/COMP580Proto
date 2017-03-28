5f5fc9219031714b380dab7f10e2faf281cffe4d
Justin Baldwin <Justin Baldwin>
2017-03-27 21:13:24 -0400

    added functionality for TextToSpeech, need to call stringToSpeech method in MainActivity to make it talk

--- app/src/main/java/edu/unc/miller/comp580proto/MainActivity.java
+++ app/src/main/java/edu/unc/miller/comp580proto/MainActivity.java
@@ -2,15 +2,20 @@ package edu.unc.miller.comp580proto;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.speech.tts.TextToSpeech.*;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements OnInitListener{
 
     RelativeLayout rl;
+    TextToSpeech tts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         super.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LOCKED);
         initialize();
+        Intent checkTTS = new Intent();
+        checkTTS.setAction(Engine.ACTION_CHECK_TTS_DATA);
+        startActivityForResult(checkTTS, 0); //check if tts is available
     }
 
     public void mainButtonClicked(View view){
@@ -41,6 +49,19 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void onActivityResult (int requestCode, int resultCode, Intent data) {
+        if (requestCode == 0) {
+            if (resultCode == Engine.CHECK_VOICE_DATA_PASS) {
+                tts = new TextToSpeech(this, this); //initialize tts if available
+            }
+        }
+    }
+
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            tts.setLanguage(Locale.US); //set language to US English
+        }
+    }
     public void initialize(){
         updateUserString();
         //Enter immersive mode on app start
@@ -55,10 +76,16 @@ public class MainActivity extends AppCompatActivity {
 
     //Set the text of the edit text box
     public void updateUserString(){
-        if(Vars.userString!=null){
+        if(Vars.userString != null){
             EditText editText = (EditText)findViewById(R.id.main_text_field);
             if(editText!=null) editText.setText(Vars.userString);
         }
+    }
+
+    //call to push Vars.userString to be spoken
+    public void stringToSpeech() {
+        tts.speak(Vars.userString, TextToSpeech.QUEUE_ADD, null);
+        Vars.userString = ""; //reset userString after spoken
     }
 
     @Override
